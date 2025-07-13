090c9887ba161cd34aff5af60b0dd444e5025f36
Dandefensor <ad79702@gmail.com>
2025-07-13 13:54:52 -0400

    Se agrego la lista de productos y formulario

--- coffe_shop/products/urls.py
+++ coffe_shop/products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path 
 from products.views import product_list
-
+from products.views import ProductFormView
 
 
 
 urlpatterns = [
-    path('lista-producto/', product_list, name='Lista productos')
+    path('lista/', product_list.as_view(), name='product_list'),
+    path('agregar/', ProductFormView.as_view(), name="add_product")
 ]
--- coffe_shop/products/views.py
+++ coffe_shop/products/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.views import generic
+from products.forms import product_form
+from products.models import Product
 
 
 # Create your views here.
 
-def product_list(request):
-    contexto = {'nombre': 'Andrés'}
+class ProductFormView(generic.FormView):
+    template_name = "add_product/add_product.html"
+    form_class = product_form
+    success_url = reverse_lazy('add_product')
 
-    return render(request, 'products_list/products_list.html', contexto)
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
+
+
+class product_list(generic.ListView):
+    model = Product
+    template_name = 'products_list/products_list.html'
+    context_object_name = 'products'
+
+
+
 
