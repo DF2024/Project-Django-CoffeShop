7fa0d001ae61c3dcc7222a3a8a01e1a8663847e3
Dandefensor <ad79702@gmail.com>
2025-07-13 21:35:32 -0400

    Se agrego el registro de usuarios

--- coffe_shop/users/views.py
+++ coffe_shop/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib import messages
 
-# Create your views here.
+
+
+
+class RegisterView(CreateView):
+    template_name = "users/register.html"
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        messages.success(self.request, '¡Usuario creado con éxito! Ya puedes iniciar sesión.')
+        return super().form_valid(form)
+    
